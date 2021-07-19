2488a216809737387a9630e4ab74b1b6abb9c0a4
renumokalkar <68773132+renumokalkar@users.noreply.github.com>
2021-07-19 20:20:07 +0530

    Add files via upload

---
+++ inputfunc.py
@@ -0,0 +1,44 @@
+# name=input("enter your name\n")
+# print("result sheet for first semester")
+# sub1=input("enter first subject\n")
+# sub2=input("enter second subject\n")
+# sub3=input("enter third subject\n")
+# sub4=input("enter fourth subject\n")
+# sub5=input("enter fifth subject\n")
+#
+# mark1=int(input(f"enter {sub1} marks\n"))
+# mark2=int(input(f"enter {sub2} marks\n"))
+# mark3=int(input(f"enter {sub3} marks\n"))
+# mark4=int(input(f"enter {sub4} marks\n"))
+# mark5=int(input(f"enter {sub5} marks\n"))
+#
+# total=mark1+mark2+mark3+mark4+mark5
+# avg=total/5
+# percentage=((total/500)*100)
+# print(total)
+# print(avg)
+# print(percentage)
+#
+# print("result sheet for second semester")
+# sub_1=input("enter first subject\n")
+# sub_2=input("enter second subject\n")
+# sub_3=input("enter third subject\n")
+# sub_4=input("enter fourth subject\n")
+# sub_5=input("enter fifth subject\n")
+#
+# mark_1=int(input(f"enter {sub_1} marks\n"))
+# mark_2=int(input(f"enter {sub_2} marks\n"))
+# mark_3=int(input(f"enter {sub_3} marks\n"))
+# mark_4=int(input(f"enter {sub_4} marks\n"))
+# mark_5=int(input(f"enter {sub_5} marks\n"))
+#
+# total=mark_1+mark_2+mark_3+mark_4+mark_5
+# avg=total/5
+# percentage=((total/500)*100)
+# print(total)
+# print(avg)
+# print(percentage)
+
+
+
+
